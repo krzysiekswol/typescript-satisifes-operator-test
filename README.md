0c8f28f3c9ca175159bba59ff60a5bd36f2927d6
krzysiekswol <64749798+krzysiekswol@users.noreply.github.com>
2023-09-30 21:01:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-satisifes-operator-test
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-jtu71c)
---
+++ index.ts
@@ -0,0 +1,52 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+
+type personName = 'Jan' | 'Ala' | 'Marta';
+
+type personOtherInfo = {
+  id: number;
+  age: number;
+}
+
+type personInfo = personName | personOtherInfo;
+
+type Person = {
+  name: personInfo;
+  details: personInfo;
+}
+
+// Old solution before ts 4.9
+const studentFirst: Person = {
+  name: 'Ala',
+  details: {
+    id: 2,
+    age: 20,
+  },
+};
+
+if (typeof studentFirst.name === 'string') {
+  studentFirst.name.toUpperCase();
+};
+
+
+const student = {
+  name: 'Jan',
+  details: { id: 1, age: 24 }, 
+} satisfies Person;
+
+student.name.toLowerCase();
+
+const studentThree = {
+  name: 'Jan',
+  details: {
+    id: 1,
+    age: true,
+  }, 
+} satisfies Person;
+
+studentThree.name.toLowerCase();
